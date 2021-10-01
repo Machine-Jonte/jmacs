5a13f1453be8e8200b1df7d50620d3d1e53d483e
Jonathan Oesterberg <jonte_ost@live.com>
2021-10-01 16:42:52 +0800

    Add forge

--- init.el
+++ init.el
@@ -251,6 +251,8 @@
   (setq projectile-switch-project-action #'projectile-dired)
   (setq projectile-enable-caching nil))
 
+(use-package forge)
+
 ; (use-package counsel-projectile
 ;   :config (counsel-projectile-mode))
 
@@ -289,7 +291,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(objed-cursor-color "#ff6c6b")
  '(package-selected-packages
-   '(evil-collection evil-magit counsel-projectile projectile org-superstar org-appear smooth-scrolling exec-path-from-shell general helpful ivy-rich which-key rainbow-delimiters doom-modeline doom-themes counsel ayu-theme use-package swiper lsp-mode evil command-log-mode))
+   '(forge evil-collection evil-magit counsel-projectile projectile org-superstar org-appear smooth-scrolling exec-path-from-shell general helpful ivy-rich which-key rainbow-delimiters doom-modeline doom-themes counsel ayu-theme use-package swiper lsp-mode evil command-log-mode))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(rustic-ansi-faces
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
